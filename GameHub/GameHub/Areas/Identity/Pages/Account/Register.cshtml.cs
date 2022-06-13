dfe87c4d0f181bfc15878bb2fb52d80ec6868beb
vgrbo1 <vgrbo1@etf.unsa.ba>
2022-06-13 20:58:47 +0200

    Popravka zapisa uloge.

--- GameHub/GameHub/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ GameHub/GameHub/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -102,7 +102,7 @@ public class InputModel
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
-
+                    await _userManager.AddToRoleAsync(user, Input.UserRole);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
