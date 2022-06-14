8ba3626dfb7dc72effb56734f437b9d8887ca5ab
beganov1c <tbeganovic1@etf.unsa.ba>
2022-06-14 15:15:44 +0200

    Forma za registraciju i prijavu popravljena.

--- GameHub/GameHub/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ GameHub/GameHub/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -44,7 +44,6 @@ namespace GameHub.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [EmailAddress]
             public string Email { get; set; }
 
             [Required]
