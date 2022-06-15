3b5d9a17ee648fa625ae11455d40f79165616d41
vgrbo1 <vgrbo1@etf.unsa.ba>
2022-06-15 16:42:38 +0200

    Ispravka ukoliko korisnik nema sliku.

--- GameHub/GameHub/Startup.cs
+++ GameHub/GameHub/Startup.cs
@@ -32,7 +32,7 @@ namespace GameHub
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false).
                 AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
